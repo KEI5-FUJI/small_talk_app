1754f222d0d169a14e1f1735ad92f7d16a6c11ef
keigo <biolemon.1010@gmail.com>
2022-01-28 19:05:15 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,21 +8,4 @@ RSpec.describe "StaticPages", type: :request do
       assert_select "title", "Home | Ghost App"
     end
   end
-
-  describe "GET /help" do
-    it "returns http success" do
-      get "/static_pages/help"
-      expect(response).to have_http_status(:success)
-      assert_select "title", "Help | Ghost App"
-    end
-  end
-
-  describe "GET /about" do
-    it "returns http success" do
-      get "/static_pages/about"
-      expect(response).to have_http_status(:success)
-      assert_select "title", "About | Ghost App"
-    end
-  end
-
 end

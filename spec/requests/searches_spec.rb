a22d0e88baf2da3c9215e841dd284ff4b89c8764
keigo <biolemon.1010@gmail.com>
2022-02-19 17:31:19 +0900

    finish search test

--- spec/requests/searches_spec.rb
+++ spec/requests/searches_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "Searches", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:first_user) {FactoryBot.create(:user)}
+  let!(:last_user) {FactoryBot.create(:user)}
+
+  describe "ログインしてユーザー検索" do
+    before do
+      log_in_as(first_user)
+    end
+
+    context "間違ったメールで検索" do
+      it "何も出てこない" do
+        get search_path, params: {keyword: "wrong email"}
+        expect(response.body).not_to include(last_user.name)
+      end
+    end
+
+    context "何も入れずに検索" do
+      it "何も出てこない" do
+        get search_path, params: {keyword: ""}
+        expect(response.body).not_to include(last_user.name)
+      end
+    end
+
+    context "正しいメールで検索" do
+      it "何も出てこない" do
+        get search_path, params: {keyword: last_user.email}
+        expect(response.body).to include(last_user.name)
+      end
+    end
   end
+
+  describe "ログインせずに" do
+    it "入れない" do
+      get search_path
+      expect(response).to redirect_to login_url
+    end
+  end
+  
+  
 end

be82ba2de9e28a561e2106ea60012df1b1bd3381
keigo <biolemon.1010@gmail.com>
2022-02-20 00:39:07 +0900

    finish all

--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "PasswordResets", type: :request do
   describe "パスワードリセット" do
+    let(:user) {FactoryBot.create(:user)}
     before do
-      ActionMailer::Base.deliveries.clear
-      @user = FactoryBot.create(:user)
+      user.create_reset_digest
     end
 
     context "メールアドレスが無効な時" do
@@ -20,8 +20,8 @@ RSpec.describe "PasswordResets", type: :request do
     context "パスワード変更メールアドレスが有効な時" do
       it "ルートに行く" do
         get new_password_reset_url
-        post password_resets_url, params: {password_reset: {email: @user.email}}
-        expect(@user.reset_digest).not_to eq @user.reload.reset_digest
+        post password_resets_url, params: {password_reset: {email: user.email}}
+        expect(user.reset_digest).not_to eq user.reload.reset_digest
         expect(ActionMailer::Base.deliveries.size).to eq 1
         expect(flash.empty?).to eq false
         expect(response).to redirect_to root_url
@@ -30,79 +30,68 @@ RSpec.describe "PasswordResets", type: :request do
       context "メールが無効" do
         it "ルートに戻る" do
           get new_password_reset_url
-          post password_resets_url, params: {password_reset: {email: @user.email}}
-          get edit_password_reset_url(@user, email: "")
+          post password_resets_url, params: {password_reset: {email: user.email}}
+          get edit_password_reset_url(user, email: "")
           expect(response).to redirect_to root_url 
         end 
       end
 
       context "ユーザーが無効" do
         it "ルートに戻る" do
-          @user.toggle!(:activated)
+          user.toggle!(:activated)
           get new_password_reset_url
-          post password_resets_url, params: {password_reset: {email: @user.email}}
-          get edit_password_reset_url(@user, email: @user.email)
+          post password_resets_url, params: {password_reset: {email: user.email}}
+          get edit_password_reset_url(user, email: user.email)
           expect(response).to redirect_to root_url 
-          @user.toggle!(:activated)
+          user.toggle!(:activated)
         end
       end
 
       context "メールが有効、トークンが無効" do
         it "ルートに戻る" do
           get new_password_reset_url
-          post password_resets_url, params: {password_reset: {email: @user.email}}
-          get edit_password_reset_url("wrong token", email: @user.email)
+          post password_resets_url, params: {password_reset: {email: user.email}}
+          get edit_password_reset_url("wrong token", email: user.email)
           expect(response).to redirect_to root_url
         end
       end
 
       context "全部有効" do
         it "パスワード編集ページへ行く" do
-          get new_password_reset_url
-          post password_resets_url, params: {password_reset: {email: @user.email}}
-          get edit_password_reset_url(@user, email: @user.email)
-          assert_select "input[name=email][type=hidden][value=?]", @user.email
+          get edit_password_reset_url(user.reset_token, email: user.email)
+          assert_select "input[name=email][type=hidden][value=?]", user.email
         end
       end
 
       context "無効なパスワード" do
         it "警告文が出る。元のページにレンダー" do
-          get new_password_reset_url
-          post password_resets_url, params: {password_reset: {email: @user.email}}
-          get edit_password_reset_url(@user, email: @user.email)
-          patch password_reset_url(@user),
-          params: { email: @user.email,
+          patch password_reset_url(user.reset_token),
+          params: { email: user.email,
                     user: { password:              "foobaz",
                             password_confirmation: "barquux" } }
-          assert_select 'div#error_explanation'
+          expect(response.body).to include("パスワードリセット")
         end
       end
 
       context "パスワードが空白" do
         it "警告文が出る。元のページにレンダー" do
-          get new_password_reset_url
-          post password_resets_url, params: {password_reset: {email: @user.email}}
-          get edit_password_reset_url(@user, email: @user.email)
-          patch password_reset_url(@user),
-          params: { email: @user.email,
+          patch password_reset_url(user.reset_token),
+          params: { email: user.email,
                     user: { password:              "",
                             password_confirmation: "" } }
-          assert_select 'div#error_explanation'
+          expect(response.body).to include("パスワードリセット")
         end
       end
 
       context "正しいパスワード" do
         it "ログインして個人ページへ" do
-          get new_password_reset_url
-          post password_resets_url, params: {password_reset: {email: @user.email}}
-          get edit_password_reset_url(@user, email: @user.email)
-          patch password_reset_url(@user),
-          params: { email: @user.email,
+          patch password_reset_url(user.reset_token),
+          params: { email: user.email,
                     user: { password:              "foobaz",
                             password_confirmation: "foobaz" } }
           expect(is_logged_in?).to eq true
           expect(flash.empty?).to eq false
-          expect(response).to redirect_to @user
+          expect(response).to redirect_to user
         end
       end
 
